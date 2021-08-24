a8a299a2ed2dd09455eef95f26b9b20f7b1e614a
jampan02 <kusudatu@yahoo.co.jp>
2021-08-24 22:29:48 +0900

    add validation

--- src/scenes/signIn/index.tsx
+++ src/scenes/signIn/index.tsx
@@ -1,19 +1,20 @@
 import React, { useEffect, useState } from "react";
-import { useForm } from "react-hook-form";
+import { DeepMap, FieldError, useForm } from "react-hook-form";
 import { Link, useHistory } from "react-router-dom";
 import { useCurrentAccount } from "../../hooks/useCurrentAccount";
 import styles from "./style.module.scss";
 import { SignInParams, useSignInPresenter } from "./useSignInPresenter";
 import eye from "../signIn/Vector.png";
+import { Account } from "../../data/Account";
 
 const SignInPage = () => {
   //パスワード用inputのtypeを判定するためのState
   const [isRevealPassword, setIsRevealPassword] = useState(false);
-  const { register, handleSubmit } = useForm<SignInParams>();
+  const { register, handleSubmit, errors } = useForm<SignInParams>();
   const { signIn } = useSignInPresenter();
   const { account } = useCurrentAccount();
   const history = useHistory();
-  console.log(window.location.origin);
+  console.log(errors);
   useEffect(() => {
     if (account) history.push("/");
   }, [account]);
@@ -21,6 +22,40 @@ const SignInPage = () => {
   const onSubmit = (data: SignInParams) => {
     signIn(data);
   };
+
+  //エラーメッセージ取得関数
+  const getErrorMessage = (
+    args: DeepMap<
+      {
+        email: string;
+        password: string;
+      },
+      FieldError
+    >
+  ) => {
+    if (args.email) {
+      //emailにエラーがある場合
+      switch (args.email.type) {
+        case "required":
+          return "メールアドレスは必須です";
+        case "pattern":
+          return "メールアドレスの形式が不正です";
+        default:
+          return null;
+      }
+    } else if (args.password) {
+      //passwordにエラーがある場合
+      switch (args.password.type) {
+        case "required":
+          return "パスワードは必須です";
+        case "minLength":
+          return "パスワードは最低6文字です";
+        default:
+          return null;
+      }
+    }
+  };
+
   return (
     <div className={styles.page}>
       <div className={styles.container}>
@@ -33,7 +68,14 @@ const SignInPage = () => {
                 name="account.email"
                 type="email"
                 placeholder="coadmap@mail.com"
-                ref={register}
+                ref={register({
+                  required: true,
+                  pattern: {
+                    value:
+                      /^[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/,
+                    message: "メールアドレスの形式が不正です",
+                  },
+                })}
               />
             </div>
           </div>
@@ -44,7 +86,7 @@ const SignInPage = () => {
                 name="account.password"
                 placeholder="パスワードを入力"
                 type={isRevealPassword ? "text" : "password"}
-                ref={register}
+                ref={register({ required: true, minLength: 6 })}
               />
               <img
                 src={eye}
@@ -57,6 +99,13 @@ const SignInPage = () => {
               />
             </div>
           </div>
+          {
+            //パスワード、メールアドレスのバリデーションエラー表示
+            errors.account && (
+              <p className={styles.error}>{getErrorMessage(errors.account)}</p>
+            )
+          }
+
           <div className={styles.login}>
             <button type="submit" className={styles.submit_button_text}>
               ログイン
